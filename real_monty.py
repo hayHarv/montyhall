61cede5a12bc570a25b02005b49bf086aa9f4ecd
Hayden Harvey <haydenh@yesler.com>
2017-08-23 10:10:20 -0700

    Changed list comprehensions to np.where statements for faster code (it's way faster)

--- real_monty.py
+++ real_monty.py
@@ -27,14 +27,16 @@ class Game:
     """
     def __init__(self, player_type="stay", n_doors=3):
         self.player_type = player_type
-        self.doors = list(range(n_doors))
+        self.doors = np.arange(n_doors)
         self.winner = choose(self.doors)
         self.guess = choose(self.doors)
         self.result = 0
+
     def play(self):
-        drop_options = [i for i in self.doors if (i != self.guess) & (i != self.winner)]
+        drop_options = self.doors[np.where((self.doors != self.guess) & (self.doors !=
+            self.winner))]
         to_drop = np.random.choice(drop_options, size=1)[0]
-        new_doors = [i for i in self.doors if i != to_drop]
+        new_doors = self.doors[np.where(self.doors != to_drop)]
         if self.player_type == "stay":
             if self.guess == self.winner:
                 self.result = 1
@@ -43,7 +45,7 @@ class Game:
                 self.result = 0
                 return self.result
         elif self.player_type == "switch":
-            new_guess = choose([i for i in new_doors if i != self.guess])
+            new_guess = choose(new_doors[np.where(new_doors != self.guess)])
             if new_guess == self.winner:
                 self.result = 1
                 return self.result
@@ -59,6 +61,7 @@ class N_trials:
         self.outcomes = {}
         self.outcome_proba = {}
         self.n_doors = n_doors
+ 
     def run_trials(self):
         for player_type in ["stay", "switch"]:
             for trial in range(self.n):
